7e58c633f9afc8458b3d0c64169fb43f7ae6ecaf
stOneskull <stOne@skull.casa>
2025-12-07 16:36:46 +1100

    labs

---
+++ 2025/07/labs.py
@@ -0,0 +1,24 @@
+with open("input.txt", "r") as file:
+    lines = file.read().splitlines()
+
+S_index = lines[0].find("S")
+
+
+beams = {S_index}
+splits = 0
+
+
+for row in lines[1:]:
+    next_beams = set()
+
+    for c in beams:
+        if row[c] == '^':
+            next_beams.add(c-1)
+            next_beams.add(c+1)
+            splits += 1
+        else:
+            next_beams.add(c)
+    
+    beams = next_beams
+
+print(splits)
---
+++ 2025/07/labs2.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+
+with open("input.txt", "r") as file:
+    lines = file.read().splitlines()
+
+S_index = lines[0].find("S")
+
+timelines = Counter({S_index: 1})
+
+
+for row in lines[1:]:
+    next_timelines = Counter()
+    for c, count in timelines.items():
+        if row[c] == '^':
+            next_timelines[c-1] += count
+            next_timelines[c+1] += count
+        else:
+            next_timelines[c] += count
+    
+    timelines = next_timelines
+
+total_timelines = sum(timelines.values())
+
+print(total_timelines)
