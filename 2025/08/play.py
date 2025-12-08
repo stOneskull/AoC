4caa9412e30482b52fc4b160250c53e2ec95ffc2
stOneskull <stOne@skull.casa>
2025-12-09 05:57:27 +1100

    no need for sqrt for distance

--- 2025/08/play.py
+++ 2025/08/play.py
@@ -1,4 +1,3 @@
-from math import sqrt
 from itertools import combinations
 from collections import Counter
 
@@ -14,7 +13,7 @@ parent = {box: box for box in boxes}
 
 
 def distance(p1, p2):
-    return sqrt(
+    return (
         (p1[0] - p2[0])**2 +
         (p1[1] - p2[1])**2 +
         (p1[2] - p2[2])**2
--- 2025/08/play2.py
+++ 2025/08/play2.py
@@ -1,4 +1,3 @@
-from math import sqrt
 from itertools import combinations
 
 
@@ -13,7 +12,8 @@ parent = {box: box for box in boxes}
 
 
 def distance(p1, p2):
-    return sqrt(
+    """Calculates the squared Euclidean distance, avoiding a costly sqrt."""
+    return (
         (p1[0] - p2[0])**2 +
         (p1[1] - p2[1])**2 +
         (p1[2] - p2[2])**2
