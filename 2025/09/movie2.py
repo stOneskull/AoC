39510338c89b701b994d4482a20f39c7add1a090
stOneskull <stOne@skull.casa>
2025-12-10 09:38:11 +1100

    shapely

--- 2025/09/movie2.py
+++ 2025/09/movie2.py
@@ -18,11 +18,12 @@ def check_rectangle(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     
-    candidate_rectangle = box(
-        min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
+    rectangle = box(
+        min(x1, x2), min(y1, y2), 
+        max(x1, x2), max(y1, y2)
         )
 
-    if theater_floor.contains(candidate_rectangle):
+    if theater_floor.contains(rectangle):
         return (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
     return 0
 
